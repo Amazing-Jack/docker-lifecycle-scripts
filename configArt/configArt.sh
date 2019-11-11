ea04167a94abd591648f2733e4e4cc7d23758d55
Jack Chung <jackchung@jfrogchina.com>
2019-11-11 15:52:35 +0800

    Updaetd configArt.sh

--- configArt/configArt.sh
+++ configArt/configArt.sh
@@ -1,6 +1,5 @@
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/swampup" -T swampup.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/apache-archive" -T apache-archive.json
-curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X POST "http://art.local:8081/artifactory/api/repositories/docker-remote" -T docker-remote.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/tomcat-local" -T tomcat-local.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/libs-dev-local" -T libs-dev-local.json
 curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/libs-dev" -T libs-dev.json
