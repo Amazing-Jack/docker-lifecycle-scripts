bbf9f6f18031d59b167b77c577116faac2002a67
Jack Chung <jackchung@jfrogchina.com>
2019-11-11 15:41:24 +0800

    Updated configArt.sh & warmup.sh

--- configArt/configArt.sh
+++ configArt/configArt.sh
@@ -1,18 +1,10 @@
-curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory
-/api/repositories/swampup" -T swampup.json
-curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory
-/api/repositories/apache-archive" -T apache-archive.json
-curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X POST "http://art.local:8081/artifactory
-/api/repositories/docker-remote" -T docker-remote.json
-curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory
-/api/repositories/tomcat-local" -T tomcat-local.json
-curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory
-/api/repositories/libs-dev-local" -T libs-dev-local.json
-curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory
-/api/repositories/libs-dev" -T libs-dev.json
-curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory
-/api/repositories/docker-prod" -T docker-prod.json
-curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory
-/api/repositories/docker-dev" -T docker-dev.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/swampup" -T swampup.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/apache-archive" -T apache-archive.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.RemoteRepositoryConfiguration+json" -X POST "http://art.local:8081/artifactory/api/repositories/docker-remote" -T docker-remote.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/tomcat-local" -T tomcat-local.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.LocalRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/libs-dev-local" -T libs-dev-local.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/libs-dev" -T libs-dev.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/docker-prod" -T docker-prod.json
+curl -uadmin:password -H "content-type: application/vnd.org.jfrog.artifactory.repositories.VirtualRepositoryConfiguration+json" -X PUT "http://art.local:8081/artifactory/api/repositories/docker-dev" -T docker-dev.json
 
 ./warmup.sh
--- configArt/warmup.sh
+++ configArt/warmup.sh
@@ -1,3 +1,2 @@
-curl -I "http://art.local:8081/artifactory/apache-archive/tomcat/tomcat-8/v8.0.32/bin/apache-tomcat-8.0.32.tar.gz"
-curl -I "http://art.local:8081/artifactory/apache-archive/tomcat/tomcat-8/v8.0.35/bin/apache-tomcat-8.0.35.tar.gz"
-curl -I "http://art.local:8081/artifactory/swampup/java/jdk-8u91-linux-x64.tar.gz"
+curl -I "http://art.local:8081/artifactory/tomcat-local/org/apache/apache-tomcat/apache-tomcat-8.0.35.tar.gz"
+curl -I "http://art.local:8081/artifactory/tomcat-local/java/jdk-8u91-linux-x64.tar.gz"
